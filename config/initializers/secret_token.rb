e1a78fc680d88636888aa32bdfd56cb81c8048ae
Robert Waltemath <aldrin@systemausfall.org>
2013-09-24 09:36:40 +0200

    Initial state after rails new.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Initiative::Application.config.secret_key_base = 'db26af56e3a4eaa59ef5af7f97e387d63f83b9bfb20f8ab178348815df422e347f14fb427e9eb434e138f291b1cf005639371c493e54ac46d688759bfd756364'
