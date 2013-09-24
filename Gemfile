182c039fee4613a64f6ebe78070b389c2e1ae1f2
Robert Waltemath <aldrin@systemausfall.org>
2013-09-24 11:23:17 +0200

    Added failing step.

--- Gemfile
+++ Gemfile
@@ -2,18 +2,8 @@ source 'https://rubygems.org'
 
 gem 'rails', '4.0.0'
 gem 'sqlite3'
-#gem 'sass-rails', '~> 4.0.0'
-#gem 'uglifier', '>= 1.3.0'
-#gem 'coffee-rails', '~> 4.0.0'
-#gem 'jquery-rails'
-#gem 'turbolinks'
-#gem 'jbuilder', '~> 1.2'
 
 group :test do
   gem 'cucumber-rails', require: false
   gem 'database_cleaner'
 end
-
-#group :doc do
-#  gem 'sdoc', require: false
-#end
---
+++ features/step_definitions/new_user_steps.rb
@@ -0,0 +1,15 @@
+Given(/^I am not visiting the site$/) do
+  # Do we need to check anything here?
+end
+
+When(/^I enter the site$/) do
+  visit root_path
+end
+
+Then(/^I should see the list of initiatives$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be able to create a new initiative$/) do
+  pending # express the regexp above with the code you wish you had
+end
