900daab6dab53fc0fade937762271baa1deb7e6a
Robert Waltemath <aldrin@systemausfall.org>
2013-10-09 12:07:51 +0200

    Finished view tests.

--- spec/views/initiatives/index.html.haml_spec.rb
+++ spec/views/initiatives/index.html.haml_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 describe "initiatives/index.html.haml" do
+  let(:initiative) { mock_model("Initiative").as_new_record.as_null_object }
+  before { assign(:initiative, initiative) }
+
   it "shows a form for initiative creation" do
-    assign(:initiative, mock_model("Initiative").as_new_record)
     render
-    expect(rendered).to have_selector('form')
-    expect(rendered).to have_selector('input[type=submit]')
+    expect(rendered).to have_selector("form[action='#{initiatives_path}'][method='post']")
+  end
+
+  it "contains a textfield for the name of the initiative" do
+    render
+    expect(rendered).to have_selector("form input[type='text']#initiative_name")
   end
 end
