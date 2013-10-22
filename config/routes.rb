b84293ce48a02af5c67239b95f0191bbe10ffa7c
Robert Waltemath <aldrin@systemausfall.org>
2013-10-22 09:02:22 +0200

    Tried devise controller spec.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Initiapp::Application.routes.draw do
   resources :initiatives, path: '', only: [:index, :create, :show]
 
-  resource :users, controller: 'devise/registrations', as: 'user_registration', only: [:create]
+  devise_for :users, only: [:registrations]
 end
---
+++ spec/controllers/registrations_controller_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+describe Devise::RegistrationsController do
+end
