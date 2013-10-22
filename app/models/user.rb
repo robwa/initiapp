11c86026f3f927e254546e9d0e3e55f896b683cc
Robert Waltemath <aldrin@systemausfall.org>
2013-10-22 10:07:12 +0200

    Added user spec.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def password_required?
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with only an email address" do
+    user = User.new(email: "test@address.email")
+    expect(user).to be_valid
+  end
 end
