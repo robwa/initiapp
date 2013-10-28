ccdcdf69bb1c9cf3c17e622c7208c124cc3cf7a9
Robert Waltemath <aldrin@systemausfall.org>
2013-10-28 11:18:44 +0100

    Complemented show view.

--- spec/views/initiatives/show.html.haml_spec.rb
+++ spec/views/initiatives/show.html.haml_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 describe "initiatives/show.html.haml" do
   let(:name) { "Test Initiative" }
-  let(:initiative) { double("Initiative", name: name) }
-
-  before do
+  let(:initiative) { double("Initiative", name: name).as_null_object }
+  
+  before(:each) do
+    allow(view).to receive(:user_signed_in?)
     assign(:initiative, initiative)
   end
 
@@ -18,9 +19,39 @@ describe "initiatives/show.html.haml" do
     expect(rendered).to have_selector('h1', text: name)
   end
 
-  it "shows a form for joining the initiative" do
-    render
-    expect(rendered).to have_selector("form[action='#{join_initiative_path(initiative)}'][method='post']")
-    expect(rendered).to have_selector("form input[type='text']#user_email")
+  context "no user is signed in" do
+    before(:each) do
+      allow(view).to receive(:user_signed_in?).and_return(false)
+    end
+
+    it "shows a form for joining the initiative" do
+      render
+      expect(rendered).to have_selector("form[action='#{join_initiative_path(initiative)}'][method='post']")
+      expect(rendered).to have_selector("form input[type='text']#user_email")
+    end
+  end
+
+  context "a user is signed in" do
+    let(:user) { double(User) }
+    before(:each) do
+      allow(view).to receive(:user_signed_in?).and_return(true)
+      allow(view).to receive(:current_user).and_return(user)
+  end
+
+    context "the user is a member of the initiative" do
+      before(:each) do
+        allow(user).to receive(:member_of?).and_return(true)
+      end
+
+      it "doesn't show a form for joining the initiative" do
+        render
+        expect(rendered).not_to have_selector("form[action='#{join_initiative_path(initiative)}']")
+      end
+
+      it "displays the list of members" do
+        render
+        expect(rendered).to have_selector("ul#members")
+      end
+    end
   end
 end
