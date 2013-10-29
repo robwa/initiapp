c463dfe279b600c97fe201c95962c094f39e13de
Robert Waltemath <aldrin@systemausfall.org>
2013-10-29 10:32:42 +0100

    Complemented and refactored show view and spec.

--- spec/views/initiatives/show.html.haml_spec.rb
+++ spec/views/initiatives/show.html.haml_spec.rb
@@ -5,7 +5,7 @@ describe "initiatives/show.html.haml" do
   let(:initiative) { double("Initiative", name: name).as_null_object }
   
   before(:each) do
-    allow(view).to receive(:user_signed_in?)
+    user_stubs
     assign(:initiative, initiative)
   end
 
@@ -20,27 +20,47 @@ describe "initiatives/show.html.haml" do
   end
 
   context "no user is signed in" do
-    before(:each) do
-      allow(view).to receive(:user_signed_in?).and_return(false)
-    end
-
     it "shows a form for joining the initiative" do
       render
       expect(rendered).to have_selector("form[action='#{join_initiative_path(initiative)}'][method='post']")
       expect(rendered).to have_selector("form input[type='text']#user_email")
     end
+
+    it "doesn't display the list of members" do
+      render
+      expect(rendered).not_to have_selector("ul#members")
+    end
   end
 
   context "a user is signed in" do
-    let(:user) { double(User) }
     before(:each) do
-      allow(view).to receive(:user_signed_in?).and_return(true)
-      allow(view).to receive(:current_user).and_return(user)
-  end
+      initiative_stubs
+    end
+
+    context "the user is not member of the initiative" do
+      before(:each) do
+        user_stubs(sign_in: true)
+      end
+
+      it "shows a form for joining the initiative" do
+        render
+        expect(rendered).to have_selector("form[action='#{join_initiative_path(initiative)}'][method='post']")
+      end
+
+      it "doesn't show a textfield for entering an email address" do
+        render
+        expect(rendered).not_to have_selector("form input#user_email")
+      end
+
+      it "doesn't display the list of members" do
+        render
+        expect(rendered).not_to have_selector("ul#members")
+      end
+    end
 
     context "the user is a member of the initiative" do
       before(:each) do
-        allow(user).to receive(:member_of?).and_return(true)
+        user_stubs(sign_in: true, member: true)
       end
 
       it "doesn't show a form for joining the initiative" do
