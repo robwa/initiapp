4d769a913ace798042afd6cf1a2659749d19fad3
Robert Waltemath <aldrin@systemausfall.org>
2013-10-29 13:32:26 +0100

    Wrapped helpers in a module.

---
+++ spec/support/stub_helpers.rb
@@ -0,0 +1,21 @@
+module StubHelpers
+  def user_stubs(options = {})
+    allow(controller).to receive(:user_signed_in?).and_return(options[:sign_in])
+    if options[:sign_in]
+      user = double(User, email: options[:email])
+      allow(controller).to receive(:current_user).and_return(user)
+      allow(user).to receive(:member_of?).and_return(options[:member])
+    end
+  end
+
+  def initiative_stubs(options = {})
+    Initiative.stub(:new).and_return(options[:initiative])
+    Initiative.stub_chain(:friendly, :find).and_return(options[:initiative])
+  end
+end
+
+RSpec.configure do |config|
+  config.include StubHelpers, type: :view
+  config.include StubHelpers, type: :controller
+  config.include StubHelpers, type: :helper
+end
