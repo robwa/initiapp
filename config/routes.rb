3fca0cf3341966372ee355d99ee52a66a48070f7
Robert Waltemath <aldrin@systemausfall.org>
2013-11-02 10:08:39 +0100

    Fixed devise routing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Initiapp::Application.routes.draw do
+  devise_for :users
+
   resources :initiatives, path: '', only: [:index, :create, :show] do
     member do
       post '', action: :join, as: :join
     end
   end
-  
-  devise_for :users
 end
--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -3,15 +3,20 @@ require 'spec_helper'
 describe "routing to users" do
   let(:reserved_words) { Initiative.friendly_id_config.reserved_words }
 
-  context "existing routes" do
+  context "for the existing routes" do
     it "routes to sign in and sign out paths" do
       expect(get: "/users/sign_in").to be_routable
       expect(delete: "/users/sign_out").to be_routable
     end
+
+    it "routes registration to registrations" do
+      expect(post: "/users").to route_to(controller: "devise/registrations",
+                                         action: "create")
+    end
   end
 
-  context "reserved words" do
-    it "reserved words contain 'users'" do
+  context "for the reserved words" do
+    it "contains 'users'" do
       expect(reserved_words).to include("users")
     end
   end
