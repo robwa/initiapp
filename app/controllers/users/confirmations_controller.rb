43984c2ac9b390cef7dcd5f32843b0f1cf0b71eb
Robert Waltemath <aldrin@systemausfall.org>
2013-11-05 14:36:07 +0100

    Added ConfirmationsController but with incomplete specs.

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,2 +1,31 @@
 class Users::ConfirmationsController < Devise::ConfirmationsController
+  # GET /
+  def show
+    self.resource = resource_class.find_by(confirmation_token: params[:confirmation_token]) if params[:confirmation_token].present?
+    super if resource.nil? or resource.confirmed?
+  end
+
+  # PATCH /
+  def confirm
+    confirmation_token = params.require(resource_name)[:confirmation_token]
+    self.resource = resource_class.find_by(confirmation_token: confirmation_token)
+    unless resource.nil?
+      if resource.update(resource_params)
+        resource.confirm!
+        set_flash_message(:notice, :confirmed)
+        redirect_to after_confirmation_path_for(resource_name, resource)
+      else
+        render :show
+      end
+    else
+      redirect_to action: :show, confirmation_token: confirmation_token
+    end
+  end
+
+
+  private
+
+  def resource_params
+    params.require(:user).except(:confirmation_token).permit(:password, :password_confirmation)
+  end  
 end
--- spec/controllers/users/confirmations_controller_spec.rb
+++ spec/controllers/users/confirmations_controller_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
 
 describe Users::ConfirmationsController do
+  describe "GET show" do
+    it "finds the user instance matching the confirmation token" do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      allow(controller).to receive(:resource_class).and_return(User)
+      expect(User).to receive(:find_by)
+      get :show, confirmation_token: 'abcde'
+    end
+  end
+
+  describe "PATCH confirm" do
+    it "redirects to show for invalid confirmation token" do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      patch :confirm, user: { confirmation_token: 'abcde' }
+      expect(response).to redirect_to(action: :show, confirmation_token: 'abcde')
+    end
+  end
 end
